5c0d03045f39cc3e0d845230ab2dd80a09c9579b
litao <tustlitao@gmail.com>
2022-01-12 18:02:20 +0800

    1.12 使用贪心解法完成每日一题，这个贪心学到了

---
+++ src/leetcode/editor/cn/[31]下一个排列.java
@@ -0,0 +1,76 @@
+//实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。 
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。 
+//
+// 必须 原地 修改，只允许使用额外常数空间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 100 
+// 
+// Related Topics 数组 双指针 👍 1481 👎 0
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums.length < 2) {
+            return;
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[j] > nums[i]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    Arrays.sort(nums, i + 1, nums.length);
+                    return;
+                }
+            }
+        }
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
